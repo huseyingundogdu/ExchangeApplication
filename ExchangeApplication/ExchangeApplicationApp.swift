dbf346eb83ae1831ffdb424ba9f67208f8c74588
Huseyin <benhuseyingundogdu@gmail.com>
2024-11-14 17:13:11 +0100

    Initial Commit

---
+++ ExchangeApplication/ExchangeApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeApplicationApp.swift
+//  ExchangeApplication
+//
+//  Created by Huseyin on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
