f589865580170534704f83f7fa278bed5edcd259
Huseyin <benhuseyingundogdu@gmail.com>
2024-11-14 17:15:04 +0100

    EXBANK-44 Create base project

--- ExchangeApplication/ContentView.swift
+++ ExchangeApplication/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        // test 
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
