014b11fd5f6cb8d666cfcc9d8c6d5aee33342c88
Huseyin <benhuseyingundogdu@gmail.com>
2024-11-14 17:28:24 +0100

    EXBANK-44 test

--- ExchangeApplication/ContentView.swift
+++ ExchangeApplication/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         // test 
+        // test2
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
